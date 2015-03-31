3072fde6b5bb5cc8aa7a1f3fe03e9db5bb7ac3ea
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-03-31 14:10:35 -0400

    Centralized error codes (sealed trait's). Modified evaluate's return type.

---
+++ src/main/scala/net/common/Error.scala
@@ -0,0 +1,19 @@
+package net.common
+
+object Error {
+
+	sealed trait LispError
+	case class ParseError(ex: Throwable) extends LispError
+	
+	sealed trait InterpretterError extends LispError
+	case object BadIfError         extends InterpretterError
+	case object BadDefineError     extends InterpretterError
+	case object EmptyExpression	   extends InterpretterError
+	case object ProcError		   extends InterpretterError
+	case object NoVarExists		   extends InterpretterError
+	case object SuccessfulDefine   extends InterpretterError
+
+	sealed trait MathError extends InterpretterError
+	case class NotAnInt(x: String) extends MathError
+
+}
